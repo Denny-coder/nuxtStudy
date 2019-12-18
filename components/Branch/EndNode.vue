93fbd62e46961c8a9cb013bb996ef775c46e53f0
zke <1160345031@qq.com>
2019-12-18 11:35:51 +0800

    feat 1.仿钉钉审核流程设置

---
+++ components/Branch/EndNode.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="add-node-btn-box">
+    <div class="add-node-btn">
+      <button>+</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name:'EndNode'
+}
+</script>
